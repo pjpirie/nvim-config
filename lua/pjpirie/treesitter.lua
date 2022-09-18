bc7654d8acd10d0ff2d0a30aea2cb31d58f0d270
Paul Pirie <pauljohnpirie@yahoo.co.uk>
2022-09-18 10:07:08 +0100

    init commit

---
+++ lua/pjpirie/treesitter.lua
@@ -0,0 +1,28 @@
+local status_ok, configs = pcall(require, 'nvim-treesitter.config')
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = 'all',
+  sync_install = false,
+  ignore_install = {''},
+  autopairs = {
+		enable = true,
+	},
+  highlight = {
+    enable = true,
+    disable = { '' },
+    additional_vim_regex_highlightin = true,
+  },
+  indent = { enable = true, disable = { 'yaml' } },
+  rainbow = {
+    enable = true,
+    disable = {''},
+    max_file_lines = nil,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  }
+} 
